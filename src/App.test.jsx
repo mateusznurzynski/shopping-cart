16a0d6121d19f5ea2b554b5343171dda0e6d8592
Mateusz Nurzyński <42718924+mateusznurzynski@users.noreply.github.com>
2024-01-14 17:32:25 +0100

    test: Set up testing

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+describe('testing library', () => {
+  it('expects true to be true', () => {
+    expect(true).toBe(true)
+  })
+
+  it('expects 0 to be falsy', () => {
+    expect(0).toBeFalsy()
+  })
+})
